82e72be52b5b93033374e66df0b2f285ff765015
qusehdgns <qusehdgns123@gmail.com>
2021-03-24 15:46:32 +0900

    use ant design at react js

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// ant design
+import 'antd/dist/antd.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
